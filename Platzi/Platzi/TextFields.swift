0601a0210826947c9518090e10a87384a77a98b6
Joaquin Segovia <joasegovia9427@gmail.com>
2022-02-06 20:56:39 -0300

    Practica de TextFields y state para binding

---
+++ Platzi/Platzi/TextFields.swift
@@ -0,0 +1,34 @@
+//
+//  TextFields.swift
+//  Platzi
+//
+//  Created by Joaquin Segovia on 6/2/22.
+//.
+
+import SwiftUI
+
+struct TextFields: View {
+    @State var num:Int=0
+    @State var textoVista:String=""
+    
+    var body: some View {
+        
+        VStack {
+            Text(textoVista+" ocacion numero: \(num)")
+                        
+            TextField("Escribe tu password", text: $textoVista)
+            
+            Button(action: {num+=1;textoVista="SegundaVista"}, label: {Text("Cambia el texto de la vista")
+            })
+        }
+    }
+}
+
+struct TextFields_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TextFields()
+                .previewInterfaceOrientation(.portrait)
+        }
+    }
+}
